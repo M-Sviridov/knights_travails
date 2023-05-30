7942d3f66a7a9c3c6a662e090fef366c5a547a11
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-30 18:06:46 +1000

    Remove method to print board as unnecessary for this project

--- board.rb
+++ board.rb
@@ -8,20 +8,5 @@ class Board
     @board = Array.new(8) { Array.new(8) }
     @knight = "\u2658"
   end
-
-  def to_s
-    board.each_with_index do |row, i|
-      row.each_with_index do |piece, j|
-        if (i + j).even?
-          print "\e[47m #{piece} \e[0m"
-        else
-          print "\e[40m #{piece} \e[0m"
-        end
-      end
-      puts
-    end
-  end
 end
 
-new = Board.new
-new.to_s
