43fa0e4a6b6e2bcd0ca5970660aba1c3dbf012aa
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-30 18:37:54 +1000

    Add method to find possible moves for the knight piece given current position

--- knight.rb
+++ knight.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
+require './board'
+
 # class representing a knight piece in the game of chess
 class Knight
-  def initialize
-    @position = [nil, nil]
+  attr_accessor :position, :moves
+
+  def initialize(coordinates)
+    @position = coordinates
+    @moves = possible_moves
+  end
+
+  def possible_moves(result = [])
+    moves = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
+
+    moves.each do |move|
+      new_x_coord = position[0] + move[0]
+      new_y_coord = position[1] + move[1]
+      result << [new_x_coord, new_y_coord] if new_x_coord.between?(0, 7) && new_y_coord.between?(0, 7)
+    end
+    result
   end
 end
+
+knight = Knight.new([0, 3])
+p knight.moves
