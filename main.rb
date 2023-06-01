58f8e4fa79f7b5aca40a4f8538df559902c14f29
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-06-01 19:37:21 +1000

    Create main file to launch and test the knight_moves method

---
+++ main.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require './board'
+
+game = Board.new
+game.knight_moves([0, 0], [1, 2])
+puts ''
+game.knight_moves([0, 0], [3, 3])
+puts ''
+game.knight_moves([3, 3], [4, 3])
+puts ''
+game.knight_moves([3, 3], [0, 0])
+puts ''
