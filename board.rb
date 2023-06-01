335b841cd3851a58b549ed7fa4f43f538ee4c3ba
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-06-01 19:36:17 +1000

    Move knight_move methods to Board class

--- board.rb
+++ board.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
+require './knight'
+
 # class representing a chess board
 class Board
   attr_accessor :board
 
   def initialize
     @board = Array.new(8) { Array.new(8) }
+  end
+
+  def knight_moves(start, destination)
+    knight = Knight.new(start)
+    queue = knight.create_children
+
+    until queue.empty?
+      # queue.each { |child| p child.position }
+      current_child = queue.shift
+
+      return current_child.shortest_path if current_child.position == destination
+
+      queue += current_child.create_children
+    end
   end
 end
