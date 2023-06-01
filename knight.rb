85d45108f1112283233e00791bad7bc4c15c98c9
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-06-01 17:43:14 +1000

    Refactor method to find possible moves and add parent instance variable

--- knight.rb
+++ knight.rb
@@ -4,36 +4,23 @@ require './board'
 
 # class representing a knight piece in the game of chess
 class Knight
-  attr_accessor :position, :moves
+  attr_accessor :position, :moves, :parent
 
-  def initialize(coordinates)
+  def initialize(coordinates, parent = nil)
     @position = coordinates
-    @moves = possible_moves(coordinates, [2,1])
+    @moves = possible_moves
+    @parent = parent
   end
 
-  def possible_moves(start, destination)
+  def possible_moves
     moves = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
-    queue = [start]
-    visited = [start]
-    current_position = nil
+    result = []
 
-    until queue.empty? || current_position == destination
-      current_position = queue.shift
-      moves.each do |move|
-        new_coordinates = [current_position[0] + move[0], current_position[1] + move[1]]
-        if new_coordinates == destination
-          visited << new_coordinates
-          return visited
-        elsif valid_move?(new_coordinates) && !visited.include?(new_coordinates)
-          queue << new_coordinates
-          visited << new_coordinates
-        end
-      end
+    moves.each do |move|
+      new_coordinates = [position[0] + move[0], position[1] + move[1]]
+      result << new_coordinates if valid_move?(new_coordinates)
     end
-  end
-
-  def show_shortest_path(start, destination)
-    possible_moves(start, destination).each { |move| p move }
+    result
   end
 
   def valid_move?(coordinates)
@@ -42,6 +29,3 @@ class Knight
   end
 end
 
-knight = Knight.new([0, 0])
-knight.moves
-knight.show_shortest_path(knight.position, [3, 3])
